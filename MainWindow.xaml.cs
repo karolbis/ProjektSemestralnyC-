f6129bf0e2b38e50ba08cddd1e7778e827e437cb
karolbis <61646274+karolbis@users.noreply.github.com>
2021-02-18 22:39:05 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektSem
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //displays window whenever application is running
+            InitializeComponent();
+        }
+
+        private void btnFirst_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 sW = new Window1();
+            sW.Show();
+            this.Close();         
+        }
+        private void btnThird_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 sW = new Window2();
+            sW.Show();
+            this.Close();
+        }
+        private void btnForth_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 sW = new Window3();
+            sW.Show();
+            this.Close();
+        }
+        private void btnFifth_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 sW = new Window4();
+            sW.Show();
+            this.Close();
+        }
+    }
+}
